054b6ff070c96ade5f6427e4fa8a0903354d7819
github username <github email address>
2018-11-23 17:26:18 -0600

    artist, author, post and song file updated

---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+require_relative "artist.rb"
+
+class Song
+attr_accessor :title, :artist 
+end
+
+#     def initialize(title)
+#         @title = title
+#     end
+# end
+
+# hotline_bling = Song.new("Hotline Bling")
